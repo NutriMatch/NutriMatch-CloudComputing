9354fbf8173701f31bd263ac51ebd1974a9a477b
Jelita <jelita.firdaussakinah.tik20@mhsw.pnj.ac.id>
2023-05-27 14:03:51 +0700

    login update

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import auth
+from firebase_admin import credentials, db, auth
 from config import secret_key
 import re
 import jwt
@@ -16,7 +14,7 @@ firebase_admin.initialize_app(cred, {
 
 # Initialize Flask
 app = Flask(__name__)
-
+CORS(app)
 
 def is_valid_email(email):
     pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
@@ -24,7 +22,7 @@ def is_valid_email(email):
 
 def create_access_token_with_claims(identity, secret_key):
     additional_claims = {
-        'custom_key': 'hctamirtun'  # Tambahkan klaim kustom sesuai kebutuhan Anda
+        'custom_key': 'hctamirtun'
     }
     payload = {
         'sub': identity,
@@ -38,7 +36,7 @@ def create_access_token_with_claims(identity, secret_key):
 @app.route('/auth/register', methods=['POST'])
 def register():
     data = request.get_json()
-    fullname = data.get('fullname')
+    fullname = data.get('fullname') 
     birthday = data.get('birthday')
     email = data.get('email')
     password = data.get('password')
@@ -59,7 +57,7 @@ def register():
     # 200: Registration
     try:
         # Create authentication 
-        auth.create_user(email=email, password=password)
+        user = auth.create_user(email=email, password=password)
         
         # Add user's data to Realtime Database
         users_ref = db.reference('users')
@@ -102,6 +100,86 @@ def register():
             'data': None
         }
         return jsonify(response), 401
+
+
+
+# LOGIN
+@app.route('/auth/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    # 400: All fields required
+    if not email or not password:
+        response = {
+            'status': False,
+            'message': 'Email and password are required fields.',
+            'data': None
+        }
+        return jsonify(response), 400
+
+    # 400: Invalid email
+    if not is_valid_email(email):
+        response = {
+            'status': False,
+            'message': 'Email Invalid!',
+            'data': None
+        }
+        return jsonify(response), 400
+
+    # 200: Login success
+    try:
+        user = auth.get_user_by_email(email)
+        user_id = user.uid
+
+        # Generate access token
+        access_token = create_access_token_with_claims(email, secret_key)
+
+        # Get user data from Realtime Database
+        users_ref = db.reference('users')
+        user_data = users_ref.order_by_child('email').equal_to(email).get()
+
+        # Get body measurement data
+        body_measurement_ref = db.reference('body_measurements')
+        query = body_measurement_ref.order_by_child('user_id').equal_to(list(user_data.keys())[0]).get()
+
+        # User response data
+        user_response = {
+            'id': 1,  # Provide the actual user ID here
+            'fullname': user_data[list(user_data.keys())[0]]['fullname'],
+            'email': email,
+            'birthday': user_data[list(user_data.keys())[0]]['birthday'],
+            'body_measurement': {
+                'height': query[list(query.keys())[0]]['height'],
+                'weight': query[list(query.keys())[0]]['weight'],
+                'activity_level': query[list(query.keys())[0]]['activity_level'],
+                'gender': query[list(query.keys())[0]]['gender']
+            }
+        }
+
+        response = {
+            'status': True,
+            'message': 'Login success!',
+            'data': {
+                'token': access_token,
+                'body_measurement_setting': True,
+                'user': user_response
+            }
+        }
+        return jsonify(response), 200
+    
+    # 401: Email or Password not found !! The response still error !!
+    except ValueError as e:
+        error_message = str(e)
+        response = {
+            'status': False,
+            'message': 'Email or password not found!',
+            'data': None
+        }
+        return jsonify(response), 401
+
+
 
 
 
